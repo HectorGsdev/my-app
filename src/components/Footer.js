580d46edff707e40e03ff6c8fa6b5387ef31b9a8
Hector Gonzalez <hector.gonzalez.seattle@gmail.com>
2023-02-17 11:29:02 -0800

    Card added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
+import Card from './Card';
+
 function Footer(){
     return(
-<h1>This is a footer</h1>
+        <Card>
+            <h1>This is a footer</h1>
+        </Card>
 )
 
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
-function Header(){
-   return (<h1>This is a header</h1>
-   )
+import Card from './Card';
+
+function Header() {
+  return (
+
+     <Card>
+        <h1>This is a header</h1>
+      </Card>
+
+  );
 }
 
-export default Header
+export default Header;
